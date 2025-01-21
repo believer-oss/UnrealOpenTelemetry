3fdbc703df3a546351032bbca90d9b473f11dddb
Reuben Dunnington <rdunnington@believer.gg>
2025-01-21 13:49:46 -0800

    chore(test): update script with docker image for convenience

--- Test/run-container.ps1
+++ Test/run-container.ps1
@@ -1,1 +1,3 @@
+docker pull otel/opentelemetry-collector-contrib:0.111.0
+docker run otel/opentelemetry-collector-contrib:0.111.0
 docker run -p 4317:4317 -v .\config.yaml:/etc/otelcol-contrib/config.yaml otel/opentelemetry-collector-contrib:0.111.0
